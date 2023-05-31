4557550bdb52f9b16538c223cb4af6f240e7a586
Toqa Zidan <toqazidan@hotmail.com>
2023-05-30 18:04:29 -0700

    0,1tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+
+        Args:
+            size (int): The size of  new square.
+        """
+        self.__size = size
