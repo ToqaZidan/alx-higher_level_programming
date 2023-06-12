e82746115fc968ddd6722e66110f320323bc1dcb
Toqa Zidan <toqazidan@hotmail.com>
2023-06-12 15:28:01 -0700

    tasks

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,3 +15,8 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+    
+    def __str__(self, size):
+        """return string representation of square"""
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+
