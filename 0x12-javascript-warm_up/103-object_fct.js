eeec8e348f969c65b1d6ffa389f854a03a9be025
ToqaZidan <toqazidan@hotmail.com>
2023-09-12 02:10:39 +0300

    ..

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-function incr () {
-  myObject.value++;
-}
-myObject.incr = incr;
-console.log(myObject);
 
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+/* Update this script by adding a new function
+    incr that increments the integer value.
+*/
+
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+    function incr() {
+      myObject.value++;
+    }
+    myObject.incr = incr;
+  
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
