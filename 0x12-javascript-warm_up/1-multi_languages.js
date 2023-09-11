68c55c9de0c88b717f1ef170da9b6f36d9d350b9
ToqaZidan <toqazidan@hotmail.com>
2023-09-12 00:45:09 +0300

    ..

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -5,4 +5,6 @@
 const myVar0 = 'C is fun';
 const myVar1 = 'Python is cool';
 const myVar2 = 'JavaScript is amazing';
-console.log(myVar0, '\n' + myVar1, '\n' + myVar2);
+console.log(myVar0);
+console.log(myVar1);
+console.log(myVar2);
