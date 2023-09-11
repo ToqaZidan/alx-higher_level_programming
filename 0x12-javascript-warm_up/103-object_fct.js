e128ff7c717678e7f29599e18110367e9f3b43da
ToqaZidan <toqazidan@hotmail.com>
2023-09-12 02:26:35 +0300

    ..

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -9,10 +9,9 @@ const myObject = {
     value: 12
   };
   console.log(myObject);
-    function incr() {
-      myObject.value++;
-    }
-    myObject.incr = incr;
+  myObject.incr = function incr() {
+    myObject.value++;
+  };
   
   myObject.incr();
   console.log(myObject);
