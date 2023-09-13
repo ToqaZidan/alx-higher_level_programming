c2db81dca5fc0d01734fea33d97585f7e69f9959
ToqaZidan <toqazidan@hotmail.com>
2023-09-13 03:20:39 +0300

    ..

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,6 +6,6 @@ const fs = require('fs');
 let content = '';
 
 content = content.concat(fs.readFileSync(process.argv[2]));
-content = content.concat('\n');
+// content = content.concat('\n');
 content = content.concat(fs.readFileSync(process.argv[3]));
 fs.writeFileSync(process.argv[4], content);
